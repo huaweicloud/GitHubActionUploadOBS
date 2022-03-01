e0de549a5b783519a98e0ba59a2803664ceab1f3
Chenyt26 <2396164919@qq.com>
2022-03-01 18:54:42 +0800

    更新特性
    Match-id-bc2ccf2f12b45eda25a523e0fb7670380ca715db

---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -l
+
+if [ -z "$INPUT_ACCESS_KEY" ]; then
+  echo "ak is not set. Quitting."
+  exit 1
+fi
+if [ -z "$INPUT_SECRET_KEY" ]; then
+  echo "sk is not set. Quitting."
+  exit 1
+fi
+pwd 
+ls -al
+yum -y install wget
+
+curl -O ${INPUT_OBSUTIL_URL}
+tar -xzvf obsutil_linux_amd64.tar.gz
+
+decompressingDirectory=`ls -l . |awk '/^d/ {print $NF}'|grep obsutil_linux_amd64`
+echo $decompressingDirectory
+# cd obsutil_linux_amd64_5.3.4
+chmod 755 $decompressingDirectory/obsutil
+ls -al
+./$decompressingDirectory/obsutil config -i=${INPUT_ACCESS_KEY} -k=${INPUT_SECRET_KEY} -e=obs.cn-north-4.myhuaweicloud.com  
+./$decompressingDirectory/obsutil cp ${INPUT_FILE_PATH} obs://hdn-github-action/ -f -r
+
+#cd /usr/local
+#wget https://obs-community.obs.cn-north-1.myhuaweicloud.com/obsutil/current/obsutil_linux_amd64.tar.gz
+#mkdir obsutil_linux_amd64
+#tar -xzvf obsutil_linux_amd64.tar.gz -C obsutil_linux_amd64 --strip-components 1
+#cd obsutil_linux_amd64
+#chmod 755 obsutil
+#ln -s /usr/local/obsutil_linux_amd64/obsutil /usr/bin/obsutil
+#/usr/bin/obsutil config -i=${INPUT_ACCESS_KEY} -k=${INPUT_SECRET_KEY} -e=obs.cn-north-4.myhuaweicloud.com  
+#/usr/bin/obsutil cp ${{ github.workspace }}/README.md obs://hdn-github-action/ -f -r
